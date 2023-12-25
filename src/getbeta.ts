faa408d4e0cb2b842fbed69706f9b7a42845c3da
ZoisLee <zois@bpcc.club>
2023-12-26 01:09:01 +0800

    fix: KV return

--- src/getbeta.ts
+++ src/getbeta.ts
@@ -1,8 +1,15 @@
 export default {
-    async fetch(request: Request, env: any) {
+    async fetch(request: Request, env: { UDON_BETA_KV: KVNamespace, R2_BUCKET: R2Bucket }) {
         const url = new URL(request.url);
         const token = url.searchParams.get('token');
-        const ifFalse: boolean = env.UDON_BETA_KV.get(token);
+
+        if (token === null) {
+            return new Response('Missing token', {
+                status: 405,
+            });
+        }
+
+        const tokenValidation = await env.UDON_BETA_KV.get(token, { type: 'text' });
 
         const fileKey = 'kareudon-win-x64.7z';
         const object = await env.R2_BUCKET.get(fileKey);
@@ -13,24 +20,17 @@ export default {
                 headers.set('Content-Disposition', 'attachment; filename="kareudon-win-x64.7z"');
                 headers.set('Content-Type', 'application/x-7z-compressed');
                 if (object !== null) {
-                    if (token === null) {
-                        return new Response('Missing token', {
-                            status: 405,
+                    if (tokenValidation === 'false') {
+                        await env.UDON_BETA_KV.put(token, 'true');
+                        return new Response(
+                            object.body, {
+                            headers: headers,
                         });
                     } else {
-
-                        if (ifFalse === false) {
-                            await env.UDON_BETA_KV.put(token, true);
-                            return new Response(
-                                object.body, {
-                                headers: headers,
-                            });
-                        } else {
-                            return new Response('Invalid token', {
-                                status: 405,
-                            });
-                        }
-                    }
+                        return new Response('Invalid token', {
+                            status: 405,
+                        });
+                    };
                 } else {
                     return new Response('Object not found', {
                         status: 405,
